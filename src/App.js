290cbd904c9dcf9faa5a35b7041c9508fc0d9752
Ben8842 <z87e99b44r55a@gmail.com>
2021-03-27 22:09:30 -0400

    first

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Row, Col } from "reactstrap";
 import Background from "./img/keyboard.jpg";
+import Background2 from "./img/keyboardtwo.jpg";
+import Background3 from "./img/keyboardthree.jpg";
+import Background4 from "./img/keyboardfour.jpg";
+import Background5 from "./img/keyboardfive.jpg";
+import Background6 from "./img/software.jpg";
+import Background7 from "./img/softwaretwo.jpg";
+import Background8 from "./img/code.jpg";
+import Background9 from "./img/computer.jpg";
 
 //var gifimg = require("./imgfolder/spinningtwo.gif").default;
 
@@ -34,6 +42,132 @@ function App() {
         <Col>Contact Me</Col>
       </Row>
       <Row className="primary">
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background2})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background3})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background4})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background5})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background6})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background7})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background8})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Col md="9" sm="0"></Col>
+        <Col>About Me</Col>
+        <Col>Projects</Col>
+        <Col>Contact Me</Col>
+      </Row>
+      <Row
+        className="secondary"
+        style={{
+          backgroundImage: `url(${Background9})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
         <Col md="9" sm="0"></Col>
         <Col>About Me</Col>
         <Col>Projects</Col>
